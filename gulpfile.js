545e10680814f401aa2eefa950cc9ffdfeaecd28
Terry Chiem <terrychiem@gmail.com>
2019-09-04 13:53:36 -0700

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var bs = require('browser-sync').create(); // create a browser sync instance.
+
+gulp.task('browser-sync', function() {
+  bs.init({
+    files: ["*.css", "*.php"],
+    proxy: "localhost/yt-classic",
+    port: 80
+  });
+});
+
+// gulp.task('watch', ['browser-sync'], function () {
+//   // gulp.watch("scss/*.scss", ['sass']);
+//   gulp.watch("*.html").on('change', bs.reload);
+// });
