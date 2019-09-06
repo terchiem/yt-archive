b332705790bc98e2cfd9b6eb4419a0ee3eb84973
Terry Chiem <terrychiem@gmail.com>
2019-09-06 16:39:28 -0700

    Change db info to external host

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,10 +1,14 @@
 <?php
-  $server = "localhost";
-  $user = "ytuser";
-  $password = "test1234";
-  $db = "yt_classic";
+  $server = "us-cdbr-iron-east-02.cleardb.net";
+  $user = "b5d2e358447bf0";
+  $password = "afcbb231";
+  $db = "heroku_503d5f6fd7b054e";
 
-  // $conn = new mysqli($server, $user, $password, $db);
+  // $server = "localhost";
+  // $user = "ytuser";
+  // $password = "test1234";
+  // $db = "yt_classic";
+
   $conn = mysqli_connect($server, $user, $password, $db);
   if (!$conn) {
     echo 'Connection Error: ' . mysqli_error($conn);
