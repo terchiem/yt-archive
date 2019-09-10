9e2a7c853350b329e7880bca829430410e89e4e1
Terry Chiem <terrychiem@gmail.com>
2019-09-10 15:24:09 -0700

    Add video list support to api calls

--- src/config/db_connect.php
+++ src/config/db_connect.php
@@ -1,15 +1,21 @@
 <?php
-  $server = "us-cdbr-iron-east-02.cleardb.net";
-  $user = "b5d2e358447bf0";
-  $password = "afcbb231";
-  $db = "heroku_503d5f6fd7b054e";
+  $production = false;
 
-  // $server = "localhost";
-  // $user = "ytuser";
-  // $password = "test1234";
-  // $db = "yt_classic";
+  if ($production) {
+    define('API_KEY', getenv('API_KEY'));
+    define('DB_SERVER', "us-cdbr-iron-east-02.cleardb.net");
+    define('DB_USER', "b5d2e358447bf0");
+    define('DB_PASSWORD', "afcbb231");
+    define('DB_NAME', "heroku_503d5f6fd7b054e");
+  } else {
+    include 'secret.php';
+    define('DB_SERVER', "localhost");
+    define('DB_USER', "ytuser");
+    define('DB_PASSWORD', "test1234");
+    define('DB_NAME', "yt_classic");
+  }
 
-  $conn = mysqli_connect($server, $user, $password, $db);
+  $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
   if (!$conn) {
     echo 'Connection Error: ' . mysqli_error($conn);
   }
--- src/helpers/api_helpers.php
+++ src/helpers/api_helpers.php
@@ -1,13 +1,30 @@
 <?php
-  include 'config/secret.php';
+  function getVideoIds($responseJson) {
+    $videoIds = [];
+    foreach ($responseJson['items'] as $item) {
+      $videoIds[] = $item['id']['videoId'];
+    }
+    return join("%2C", $videoIds);
+  }
+
+  function createVideoUri($responseJson) {
+    $videoIds = getVideoIds($responseJson);
+    $api = API_KEY;   // development
+
+    return "https://www.googleapis.com/youtube/v3/videos?"
+      ."part=snippet%2Cstatistics%2CcontentDetails"
+      ."&fields=items(id%2Csnippet(publishedAt%2CchannelId%2Ctitle%2Cdescription%2CchannelTitle%2CcategoryId%2Ctags)%2Cstatistics(viewCount%2ClikeCount%2CdislikeCount)%2CcontentDetails(duration))"
+      ."&id=$videoIds"
+      ."&key=$api";
+  }
 
   function createSearchUri($q) {
     // api parameters
     $maxResults = 50;
     $dateLimit = "2006-01-01T00%3A00%3A00Z";
     $q = rawurlencode($q);
-    // $api = API_KEY;
-    $api = getenv('API_KEY');
+    $api = API_KEY;     // development
+    // $api = getenv('API_KEY');   // production
 
     return "https://www.googleapis.com/youtube/v3/search?"
       ."part=snippet"
@@ -19,10 +36,9 @@
       ."&key=$api";
   }
 
-  function callSearchApi($q) {
+  function callApi($uri) {
     $curl = curl_init();
-    $url = createSearchUri($q);
-    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_URL, $uri);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
     
