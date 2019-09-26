964e18aa9d37cff4ee06b9d4283bff5aa7389bb0
Terry Chiem <terrychiem@gmail.com>
2019-09-26 12:28:55 -0700

    Adjust config for deployment

--- src/config/links.php
+++ src/config/links.php
@@ -1,6 +1,6 @@
 <script src="https://use.fontawesome.com/729dceb01f.js"></script>
 <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"> 
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="./style.css">
 <script
   src="https://code.jquery.com/jquery-3.4.1.min.js"
   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
